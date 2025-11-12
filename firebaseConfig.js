05ee9ee9f2990dc9a365428f6b4f40fe565434c2
Liridona Kurrumeli <kurrumelil@gmail.com>
2025-11-12 21:38:46 +0100

    .

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "VENDOS_API_KEY_KETU",
-  authDomain: "VENDOS_AUTH_DOMAIN_KETU",
-  projectId: "VENDOS_PROJECT_ID_KETU",
-  storageBucket: "VENDOS_STORAGE_BUCKET_KETU",
-  messagingSenderId: "VENDOS_SENDER_ID",
-  appId: "VENDOS_APP_ID"
-};
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
